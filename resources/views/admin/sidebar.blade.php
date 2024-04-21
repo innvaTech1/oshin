bde5aacb26d3c0f406c16dc4d0ac90adef2010f3
Rizvi Ahmed <prog.rizvi@gmail.com>
2024-04-21 19:15:59 +0600

    route changes

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -76,15 +76,8 @@
                 @include('wallet::admin.sidebar')
             @endif
 
-            @if (Module::isEnabled('ClubPoint'))
-                @include('clubpoint::admin.sidebar')
-            @endif
-
             <li class="menu-header">{{ __('Utility') }}</li>
 
-            @if (Module::isEnabled('SupportTicket') && checkAdminHasPermission('support.ticket.view'))
-                @include('supportticket::sidebar')
-            @endif
 
             @if (Module::isEnabled('NewsLetter') && checkAdminHasPermission('newsletter.view'))
                 @include('newsletter::sidebar')
--- routes/web.php
+++ routes/web.php
@@ -14,43 +14,48 @@ use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/search', function () {
-    return view('frontend.search');
-})->name('search');
-Route::get('/product/{slug}', [ProductController::class, 'show'])->name('productDetails');
-Route::get('/products', [ProductCategoryController::class, 'index'])->name('products');
-Route::get('/products/filter', [ProductCategoryController::class, 'filterProducts'])->name('filterProducts');
+// Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', function(){
+    return to_route('admin.dashboard');
+})->name('home');
 
-//COMPARE
-Route::get('/compare',[CompareController::class,'index'])->name('compare');
-Route::post('/compare',[CompareController::class,'store'])->name('compare.add');
-Route::delete('/compare/delete/{id}',[CompareController::class,'destroy'])->name('compare.delete');
+// Route::get('/search', function () {
+//     return view('frontend.search');
+// })->name('search');
+
+// Route::get('/product/{slug}', [ProductController::class, 'show'])->name('productDetails');
+// Route::get('/products', [ProductCategoryController::class, 'index'])->name('products');
+// Route::get('/products/filter', [ProductCategoryController::class, 'filterProducts'])->name('filterProducts');
+
+// //COMPARE
+// Route::get('/compare',[CompareController::class,'index'])->name('compare');
+// Route::post('/compare',[CompareController::class,'store'])->name('compare.add');
+// Route::delete('/compare/delete/{id}',[CompareController::class,'destroy'])->name('compare.delete');
 
 // Route::get('/contactus', function () {
 //     return view('frontend.contactUs');
 // })->name('contactus');
 
 // CART
-Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-Route::post('/cart/add', [CartController::class, 'store'])->name('cart.add');
-Route::delete('/cart/delete/{id}', [CartController::class, 'destroy'])->name('cart.delete');
+// Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+// Route::post('/cart/add', [CartController::class, 'store'])->name('cart.add');
+// Route::delete('/cart/delete/{id}', [CartController::class, 'destroy'])->name('cart.delete');
 
-// WISHLIST ROUTES
-Route::get('/wishlist', [WishlistController::class, 'index'])->middleware('auth')->name('wishlist');
-Route::post('/wishlist/add', [WishlistController::class, 'store'])->middleware('auth')->name('wishlist.add');
-Route::delete('/wishlist/delete/{id}', [WishlistController::class, 'destroy'])->middleware('auth')->name('wishlist.delete');
+// // WISHLIST ROUTES
+// Route::get('/wishlist', [WishlistController::class, 'index'])->middleware('auth')->name('wishlist');
+// Route::post('/wishlist/add', [WishlistController::class, 'store'])->middleware('auth')->name('wishlist.add');
+// Route::delete('/wishlist/delete/{id}', [WishlistController::class, 'destroy'])->middleware('auth')->name('wishlist.delete');
 
-
-
-// CART
-Route::get('/checkout', [CheckOutController::class, 'index'])->name('checkout');
 
 
 // CART
-Route::get('/cart', [CartController::class, 'index'])->middleware('auth')->name('cart.index');
-Route::post('/cart/add', [CartController::class, 'store'])->middleware('auth')->name('cart.add');
-Route::delete('/cart/delete/{id}', [CartController::class, 'destroy'])->middleware('auth')->name('cart.delete');
+// Route::get('/checkout', [CheckOutController::class, 'index'])->name('checkout');
+
+
+// CART
+// Route::get('/cart', [CartController::class, 'index'])->middleware('auth')->name('cart.index');
+// Route::post('/cart/add', [CartController::class, 'store'])->middleware('auth')->name('cart.add');
+// Route::delete('/cart/delete/{id}', [CartController::class, 'destroy'])->middleware('auth')->name('cart.delete');
 
 // Route::get('/checkout', function () {
 //     return view('frontend.checkout');
@@ -66,9 +71,9 @@ Route::delete('/cart/delete/{id}', [CartController::class, 'destroy'])->middlewa
 // Route::get('/aboutus', function () {
 //     return view('frontend.aboutus');
 // })->name('aboutus');
-Route::get('/notfound', function () {
-    return view('frontend.404');
-})->name('notfound');
+// Route::get('/notfound', function () {
+//     return view('frontend.404');
+// })->name('notfound');
 // Route::get('/faq', function () {
 //     return view('frontend.faq');
 // })->name('faq');
@@ -85,48 +90,48 @@ Route::get('/notfound', function () {
 // })->name('vendorDetails');
 
 // USER
-Route::get('/dashboard', function () {
-    return view('frontend.dashboard.dashboard');
-})->name('userDashboard');
+// Route::get('/dashboard', function () {
+//     return view('frontend.dashboard.dashboard');
+// })->name('userDashboard');
 
 //maintenance mode route
-Route::get('/maintenance-mode', function () {
-    $setting = Illuminate\Support\Facades\Cache::get('setting', null);
-    if (!$setting?->maintenance_mode) {
-        return redirect()->route('home');
-    }
+// Route::get('/maintenance-mode', function () {
+//     $setting = Illuminate\Support\Facades\Cache::get('setting', null);
+//     if (!$setting?->maintenance_mode) {
+//         return redirect()->route('home');
+//     }
 
-    return view('maintenance');
-})->name('maintenance.mode');
+//     return view('maintenance');
+// })->name('maintenance.mode');
 
 //Profile route
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::post('/user/update-password', [ProfileController::class, 'update_password'])->name('user.update-password');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//     Route::post('/user/update-password', [ProfileController::class, 'update_password'])->name('user.update-password');
+// });
 
-Route::get('set-language', [DashboardController::class, 'setLanguage'])->name('set-language');
+// Route::get('set-language', [DashboardController::class, 'setLanguage'])->name('set-language');
 
-Route::post('apply-coupon', [PaymentController::class, 'apply_coupon'])->name('apply-coupon');
+// Route::post('apply-coupon', [PaymentController::class, 'apply_coupon'])->name('apply-coupon');
 
-/**payment related route start */
+// /**payment related route start */
 
-Route::get('payment', [PaymentController::class, 'payment'])->name('payment');
-Route::post('pay-via-stripe', [PaymentController::class, 'pay_via_stripe'])->name('pay-via-stripe');
+// Route::get('payment', [PaymentController::class, 'payment'])->name('payment');
+// Route::post('pay-via-stripe', [PaymentController::class, 'pay_via_stripe'])->name('pay-via-stripe');
 
-Route::get('pay-via-paypal', [PaymentController::class, 'pay_via_paypal'])->name('pay-via-paypal');
+// Route::get('pay-via-paypal', [PaymentController::class, 'pay_via_paypal'])->name('pay-via-paypal');
 
-Route::post('pay-via-bank', [PaymentController::class, 'pay_via_bank'])->name('pay-via-bank');
+// Route::post('pay-via-bank', [PaymentController::class, 'pay_via_bank'])->name('pay-via-bank');
 
-Route::post('pay-via-razorpay', [PaymentController::class, 'pay_via_razorpay'])->name('pay-via-razorpay');
+// Route::post('pay-via-razorpay', [PaymentController::class, 'pay_via_razorpay'])->name('pay-via-razorpay');
 
-Route::get('pay-via-mollie', [PaymentController::class, 'pay_via_mollie'])->name('pay-via-mollie');
-Route::get('pay-via-instamojo', [PaymentController::class, 'pay_via_instamojo'])->name('pay-via-instamojo');
+// Route::get('pay-via-mollie', [PaymentController::class, 'pay_via_mollie'])->name('pay-via-mollie');
+// Route::get('pay-via-instamojo', [PaymentController::class, 'pay_via_instamojo'])->name('pay-via-instamojo');
 
-Route::get('/payment-addon-success', [PaymentController::class, 'payment_addon_success'])->name('payment-addon-success');
-Route::get('/payment-addon-faild', [PaymentController::class, 'payment_addon_faild'])->name('payment-addon-faild');
+// Route::get('/payment-addon-success', [PaymentController::class, 'payment_addon_success'])->name('payment-addon-success');
+// Route::get('/payment-addon-faild', [PaymentController::class, 'payment_addon_faild'])->name('payment-addon-faild');
 
 /**payment related route end */
 
