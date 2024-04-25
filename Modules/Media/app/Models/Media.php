cbc06d590715b8c486fc7106fd65e70b0987fca9
Rizvi Ahmed <prog.rizvi@gmail.com>
2024-04-25 21:38:27 +0600

    image path issue fix

--- Modules/Media/app/Models/Media.php
+++ Modules/Media/app/Models/Media.php
@@ -12,4 +12,14 @@ class Media extends Model {
      * The attributes that are mass assignable.
      */
     protected $fillable = ['title', 'path', 'alt_text', 'description', 'height', 'width', 'mime_type', 'size'];
+
+
+    public function getPathAttribute(){
+
+        if(file_exists(public_path($this->attributes['path']))){
+            return asset($this->attributes['path']);
+        }else if(file_exists(public_path('public/'.$this->attributes['path']))){
+            return asset('public/'.$this->attributes['path']);
+        }
+    }
 }
