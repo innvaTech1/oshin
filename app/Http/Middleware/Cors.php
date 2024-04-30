390ef20f128c60f8a6b5ce36b3da0967a252a06d
Rizvi Ahmed <prog.rizvi@gmail.com>
2024-04-30 09:21:43 +0600

    corse issue fix

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,12 +15,14 @@ class Cors
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // write middleware ware for cors issue
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-            
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        return $response;
+
 
     }
 }
