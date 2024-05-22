d66ebd0a88d6a8813eec5edd986c7f1ebccb33c7
Rizvi Ahmed <prog.rizvi@gmail.com>
2024-05-22 21:31:28 +0600

    change order controller

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -205,6 +205,7 @@ class OrderController extends Controller
             $orderDetails->save();
         }
 
+        dd(max($maxDeliveryDate));
         if (count($maxDeliveryDate) > 0) {
             $maxDeliveryDate = max($maxDeliveryDate);
             $order->order_delivery_date = now()->addDays($maxDeliveryDate);
